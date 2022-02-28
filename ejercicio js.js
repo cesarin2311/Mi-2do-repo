265797f6b70120b2232558a78c1ade4c4cde9047
César Pacheco <cesaring14@gmail.com>
2022-02-27 22:07:50 -0300

    parametros actualizados

--- ejercicio js.js
+++ ejercicio js.js
@@ -2,4 +2,4 @@ function puedeJubilarse(edad, fem, mas, aportes){
     return (edad == "fem" >= 60 && aportes >= 30) || (edad == "mas" >= 65 && aportes >= 30);
     }
 
-    console.log(65,fem,mas,33);
+    console.log(69,fem,mas,33);
